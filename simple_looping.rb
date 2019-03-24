ac5bba098f3f6dd630e92f03e521b68d757636f7
Endy Ranaudo <endyranaudo@gmail.com>
2019-03-24 08:53:04 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -11,8 +11,8 @@ def loop_iterator(number_of_times)
   phrase = "Welcome to Flatiron School's Web Development Course!"
   i = 0
   loop do
+    i += 1
     puts phrase
-    i++
     if i < number_of_times
       break
     end
